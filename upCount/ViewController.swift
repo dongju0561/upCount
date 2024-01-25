c44efba31294b281aa8d56e1f3f038de2e7e3be8
DongjuPark <pdj95044@gmail.com>
2024-01-25 16:07:31 +0900

    각 코드에 대한 설명 주석으로 추가

--- upCount/ViewController.swift
+++ upCount/ViewController.swift
@@ -15,7 +15,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // increaseButton의 탭 이벤트를 감지하여 number를 1씩 증가
+        /*
+         increaseButton의 탭 이벤트를 감지하여 number를 1씩 증가
+         increaseButton이 옵저버블(obseverable)이 됩니다. .subscribe라는 메소드를 사용해서 해당 옵저버블을 구독합니다. 여기서 구독이라고 함은 옵저버블의 이벤트를 모니터링하면서 이벤트가 발생할때 동작하는 것을 정의해줍니다.
+        */
         increaseButton.rx.tap
             .subscribe(onNext: { [weak self] in
                 guard let self = self else { return }
@@ -25,8 +28,13 @@ class ViewController: UIViewController {
 
         // number의 변화를 감지하여 레이블 업데이트
         number
+            //map 메소드로 전달받은 값을 정수형에서 문자열로 타입 변환
             .map { "\($0)" }
+        
+            //BehaviorRelay 타입을 가진 number 변수와 numberLabel과 바인딩
+            //number 변수의 값이 변경됨에 따라 UI업데이트 진행
             .bind(to: numberLabel.rx.text)
+        
             .disposed(by: disposeBag)
     }
 }
